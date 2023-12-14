387f584b36f37b030ed19caa4b0acb4adfbea2d9
al3xstana <153846254+al3xstana@users.noreply.github.com>
2023-12-14 18:16:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function sendEmail(){
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "al3xstana@gmail.com",
-        Password : "9DF552EB968844FBC9AC1FC427401831FD9E",
+        Password : "",
         To : 'al3xstana@gmail.com',
         From : "al3xstana@gmail.com",
         Subject : "This is the subject",
